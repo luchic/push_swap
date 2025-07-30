eef4ae7d0136153705f4baaf2b9d8b15cc0617e2
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-07-30 20:28:37 +0200

    Add ft_utlis.h

---
+++ ft_utils.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/30 14:32:06 by nluchini          #+#    #+#             */
+/*   Updated: 2025/07/30 14:32:40 by nluchini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_UTILS_H
+# define FT_UTILS_H
+
+int	ft_check_data(char **data, int len);
+int	ft_get_array(int **array, char **data, int len);
+
+#endif
