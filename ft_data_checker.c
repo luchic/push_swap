86c89312ee6f9a7977702f2070215d87568a24ee
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-07-30 20:26:12 +0200

    Add checking input and extract numbers to array

--- ft_data_checker.c
+++ ft_data_checker.c
@@ -6,52 +6,60 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 21:10:33 by nluchini          #+#    #+#             */
-/*   Updated: 2025/07/29 22:37:08 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/07/30 20:25:42 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-static void	ft_free_chars(char **data, size_t len)
-{
-	size_t	i;
+// static void	ft_free_chars(char **data, size_t len)
+// {
+// 	size_t	i;
 
-	if (!data)
-		return ;
-	i = 0;
-	while (i < len && data[i])
-	{
-		free(data[i]);
-		i++;
-	}
-	free(data);
-}
+// 	if (!data)
+// 		return ;
+// 	i = 0;
+// 	while (i < len && data[i])
+// 	{
+// 		free(data[i]);
+// 		i++;
+// 	}
+// 	free(data);
+// }
 
 // TODO: add checking
 int	ft_check_data(char **data, int len)
 {
-	char	ch;
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	if (!data || len <= 0)
 		return (0);
-	for (i = 0; i < len; i++)
+	i = -1;
+	j = 0;
+	while (++i < len)
 	{
-		if (!data[i])
-			return (0);
 		j = 0;
 		while (data[i][j])
 		{
-			ch = data[i][j];
-			j++;
+			if (data[i][j] == '-')
+				j++;
+			if (!ft_isdigit(data[i][j]))
+				return (0);
+			while (ft_isdigit(data[i][j]))
+				j++;
+			if (data[i][j] != ' ' && data[i][j] != '\0')
+				return (0);
+			if (data[i][j] == ' ' && data[i][j + 1] != '\0')
+				j++;
 		}
 	}
 	return (1);
 }
 
 // Function return size of total numbers i have.
-int	ft_size_data(char **data, int len)
+static int	ft_size_data(char **data, int len)
 {
 	int		size;
 	char	*ch;
@@ -68,7 +76,7 @@ int	ft_size_data(char **data, int len)
 			size++;
 		while (*ch)
 		{
-			if (*ch == ' ' && ft_isdigit(*(ch + 1)) || *ch == '-')
+			if (*ch == ' ' && (ft_isdigit(*(ch + 1)) || *(ch + 1) == '-'))
 				size++;
 			ch++;
 		}
@@ -79,11 +87,40 @@ int	ft_size_data(char **data, int len)
 
 // TODO: this is return normal data of strings
 // TODO: It's better to return allread a array of ints
-int	*ft_normalize_data(char **data, int len)
+int	ft_get_array(int **array, char **data, int len)
 {
-	int	*res;
-	int	size;
+	int		i;
+	int		j;
+	int		size;
+	char	*str;
 
+	if (!array)
+		return (-1);
 	size = ft_size_data(data, len);
-	res = ft_calloc(size, sizeof(int));
+	*array = ft_calloc(size, sizeof(int));
+	if (!*array)
+		return (-1);
+	i = 0;
+	j = 0;
+	while (i < len)
+	{
+		str = data[i];
+		while (*str)
+		{
+			while (*str == ' ' && j < size)
+				str++;
+			if (ft_isdigit(*str) || (*str == '-' && ft_isdigit(*(str + 1))))
+			{
+				(*array)[j++] = ft_atoi(str);
+				if (*str == '-')
+					str++;
+				while (ft_isdigit(*str))
+					str++;
+			}
+			else if (*str)
+				str++;
+		}
+		i++;
+	}
+	return (size);
 }
