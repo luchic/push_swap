a7997048528e8fbf2c16bb9946ee697a397a1c94
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-01 15:47:56 +0200

    Add normaliztion of array

--- includes/ft_utils.h
+++ includes/ft_utils.h
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 14:32:06 by nluchini          #+#    #+#             */
-/*   Updated: 2025/08/01 15:43:28 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/01 15:46:29 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 int	ft_check_data(char **data, int len);
 int	ft_get_array(int **array, char **data, int len);
 int	ft_check_dublicate(int *array, int size);
+int	*ft_normalize(int *array, int size);
 
 #endif
