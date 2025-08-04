e7cb73867338ef210a36114be7aa26d111e9ac47
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-04 11:42:19 +0200

    Add structer of chunckst to header

--- includes/ft_sort.h
+++ includes/ft_sort.h
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 21:14:19 by nluchini          #+#    #+#             */
-/*   Updated: 2025/07/31 21:15:32 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/03 19:37:13 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 # define FT_SORT_H
 
 # include "ft_stack.h"
+
+typedef struct s_chunks
+{
+	int	min_min;
+	int	min_max;
+	int	mid_min;
+	int	mid_max;
+	int	max_min;
+	int	max_max;
+	int	size;
+}		t_chunks;
 
 void	ft_sort(t_stack *stack_a, t_stack *stack_b);
 
