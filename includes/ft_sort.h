a3e984ced3ca28c6202c0121e8af53378f62c7c4
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-04 21:09:19 +0200

    Edit ft_sort prototype

--- includes/ft_sort.h
+++ includes/ft_sort.h
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 21:14:19 by nluchini          #+#    #+#             */
-/*   Updated: 2025/08/03 19:37:13 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/04 17:59:34 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,13 @@ typedef struct s_chunks
 	int	max_max;
 	int	size;
 }		t_chunks;
+
+typedef enum e_pos
+{
+	TOP_A,
+	TOP_B,
+	BOTTOM_B,
+} t_pos;
 
 void	ft_sort(t_stack *stack_a, t_stack *stack_b);
 
