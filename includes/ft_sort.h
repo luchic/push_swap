5b2e0240801f0d800c8d277a8431f588329f2249
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-06 20:55:44 +0200

    Edit the nore rule in file ft_sort.h

--- includes/ft_sort.h
+++ includes/ft_sort.h
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 21:14:19 by nluchini          #+#    #+#             */
-/*   Updated: 2025/08/06 16:02:57 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/06 20:55:10 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,42 @@
 
 # include "ft_stack.h"
 
-
 typedef enum e_pos
 {
 	TOP_A,
 	BOTTOM_A,
 	TOP_B,
 	BOTTOM_B,
-} t_pos;
+}			t_pos;
 
 typedef enum e_type
 {
 	MIN,
 	MID,
 	MAX,
-} t_type;
+}			t_type;
 
 typedef struct s_chunk
 {
-	t_pos pos;
-	t_type type;
-	int min;
-	int max;
-} t_chunk;
+	t_pos	pos;
+	int		min;
+	int		max;
+}			t_chunk;
 
 typedef struct s_chunks
-{	
-	t_chunk min;
-	t_chunk mid;
-	t_chunk max;
-	int	min_min;
-	int	min_max;
-	int	mid_min;
-	int	mid_max;
-	int	max_min;
-	int	max_max;
-	int	size;
-}		t_chunks;
+{
+	t_chunk	min;
+	t_chunk	mid;
+	t_chunk	max;
+	int		min_min;
+	int		min_max;
+	int		mid_min;
+	int		mid_max;
+	int		max_min;
+	int		max_max;
+	int		size;
+}			t_chunks;
 
-
-
-void	ft_sort(t_stack *stack_a, t_stack *stack_b);
+void		ft_sort(t_stack *stack_a, t_stack *stack_b);
 
 #endif
