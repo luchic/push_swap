14be0f27d1753237105a8245f3d17507d076dc5a
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-06 16:06:50 +0200

    Add new struckts

--- includes/ft_sort.h
+++ includes/ft_sort.h
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 21:14:19 by nluchini          #+#    #+#             */
-/*   Updated: 2025/08/06 13:37:19 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/06 16:02:57 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,35 @@
 
 # include "ft_stack.h"
 
-typedef struct s_chunks
+
+typedef enum e_pos
 {
+	TOP_A,
+	BOTTOM_A,
+	TOP_B,
+	BOTTOM_B,
+} t_pos;
+
+typedef enum e_type
+{
+	MIN,
+	MID,
+	MAX,
+} t_type;
+
+typedef struct s_chunk
+{
+	t_pos pos;
+	t_type type;
+	int min;
+	int max;
+} t_chunk;
+
+typedef struct s_chunks
+{	
+	t_chunk min;
+	t_chunk mid;
+	t_chunk max;
 	int	min_min;
 	int	min_max;
 	int	mid_min;
@@ -26,13 +53,7 @@ typedef struct s_chunks
 	int	size;
 }		t_chunks;
 
-typedef enum e_pos
-{
-	TOP_A,
-	BOTTOM_A,
-	TOP_B,
-	BOTTOM_B,
-} t_pos;
+
 
 void	ft_sort(t_stack *stack_a, t_stack *stack_b);
 
