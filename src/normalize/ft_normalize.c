5d162971a62a307c9858ae5fe3b158516e691c8d
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-10 11:28:25 +0200

    Move src/ft_normalization.c to src/normalize/ft_normalize.c

--- src/normalize/ft_normalize.c
+++ src/normalize/ft_normalize.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_normalization.c                                 :+:      :+:    :+:   */
+/*   ft_normalize.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 15:28:12 by nluchini          #+#    #+#             */
-/*   Updated: 2025/08/01 15:56:26 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/10 11:27:36 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int ft_get_weight(int num, int *array, int size)
+static int	ft_get_weight(int num, int *array, int size)
 {
-	int weight;
-	int i;
+	int	weight;
+	int	i;
 
 	weight = 0;
 	i = -1;
@@ -27,14 +27,13 @@ static int ft_get_weight(int num, int *array, int size)
 	return (weight);
 }
 
-int *ft_normalize(int *array, int size)
+int	*ft_normalize(int *array, int size)
 {
-	int *normalized_array;
-	int i;
-	
+	int	*normalized_array;
+	int	i;
+
 	if (!array || size <= 0)
 		return (NULL);
-	
 	normalized_array = ft_calloc(size, sizeof(int));
 	if (!normalized_array)
 		return (NULL);
