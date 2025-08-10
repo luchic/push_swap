3ae1076582632a205d492aba65ec1e5a82d754c5
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-10 19:18:56 +0200

    Add main for checker

---
+++ src/checker/main.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 16:56:12 by nluchini          #+#    #+#             */
+/*   Updated: 2025/08/10 19:12:08 by nluchini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_sort.h"
+#include "ft_stack.h"
+#include "libft.h"
+#include "ft_parse.h"
+#include "ft_normalize.h"
+#include "ft_error.h"
+#include <stdlib.h>
+
+void	print_error(void)
+{
+	ft_printf(ERROR_MESSAGE);
+}
+
+void	ft_start(int *array, int size)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+
+	stack_a = ft_init_stack(array, size);
+	if (!stack_a)
+		return (ft_error_message());
+	stack_b = ft_init_stack(NULL, 0);
+	if (!stack_b)
+		return (ft_error_message(), ft_free_stack(stack_a));
+	ft_sort(stack_a, stack_b);
+	ft_free_stack(stack_b);
+	ft_free_stack(stack_a);
+}
+
+int	main(int argc, char **argv)
+{
+	int	*arra;
+	int	*norm;
+	int	size;
+
+	if (argc < 2)
+		return (ft_error_message(), 0);
+	if (!ft_validate_args(argv + 1, argc - 1))
+		return (ft_error_message(), 0);
+	arra = NULL;
+	size = ft_get_array(&arra, argv + 1, argc - 1);
+	if (!arra)
+		return (ft_error_message(), 0);
+	if (ft_check_dublicate(arra, size))
+		return (ft_error_message(), free(arra), 0);
+	norm = ft_normalize(arra, size);
+	free(arra);
+	if (!norm)
+		return (ft_error_message(), 0);
+	ft_start(norm, size);
+	free(norm);
+	return (0);
+}
