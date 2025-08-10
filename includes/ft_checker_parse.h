b14c0e0561e1bd32f25de7a98102601c32280853
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-10 19:19:30 +0200

    Add includes for new checker

---
+++ includes/ft_checker_parse.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_checker_parse.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 19:06:00 by nluchini          #+#    #+#             */
+/*   Updated: 2025/08/10 19:06:51 by nluchini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CHECKER_PARSE_H
+# define FT_CHECKER_PARSE_H
+# include "ft_def.h"
+
+char	*ft_read_line_stdin(void);
+int		exec_instr(const char *op, t_stack *a, t_stack *b);
+
+#endif
