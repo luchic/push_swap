12a182e9dc6da868d9858cf2a69c5758e04a4b10
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-10 13:51:04 +0200

    Edit the norm in parse

--- src/parse/ft_get_array.c
+++ src/parse/ft_get_array.c
@@ -6,11 +6,13 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 11:29:24 by nluchini          #+#    #+#             */
-/*   Updated: 2025/08/10 12:03:42 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/10 13:49:42 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
+#include <stdlib.h>
 
 static int	ft_size_data(char **data, int len)
 {
@@ -38,10 +40,38 @@ static int	ft_size_data(char **data, int len)
 	return (size);
 }
 
-static void	ft_fill_array(char **data, int *array, int ldata, int larray)
+static int	ft_fill_int(char *str, int *array, int index, int size)
+{
+	char	*tmp;
+
+	while (*str)
+	{
+		while (*str == ' ' && index < size)
+			str++;
+		if (ft_isdigit(*str) || (*str == '-' && ft_isdigit(*(str + 1))))
+		{
+			array[index++] = ft_atoi(str);
+			tmp = ft_itoa(array[index - 1]);
+			if (ft_strncmp(tmp, str, ft_strlen(tmp)) != 0
+				&& str[ft_strlen(tmp)] != ' ' && str[ft_strlen(tmp)] != '\0')
+				return (free(tmp), -1);
+			free(tmp);
+			if (*str == '-')
+				str++;
+			while (ft_isdigit(*str))
+				str++;
+		}
+		else if (*str)
+			str++;
+	}
+	return (0);
+}
+
+static void	ft_fill_array(char **data, int **array, int ldata, int larray)
 {
 	int		i;
 	int		j;
+	int		index;
 	char	*str;
 
 	i = -1;
@@ -49,21 +79,14 @@ static void	ft_fill_array(char **data, int *array, int ldata, int larray)
 	while (++i < ldata)
 	{
 		str = data[i];
-		while (*str)
+		index = ft_fill_int(str, *array, j, larray);
+		if (index == -1)
 		{
-			while (*str == ' ' && j < larray)
-				str++;
-			if (ft_isdigit(*str) || (*str == '-' && ft_isdigit(*(str + 1))))
-			{
-				array[j++] = ft_atoi(str);
-				if (*str == '-')
-					str++;
-				while (ft_isdigit(*str))
-					str++;
-			}
-			else if (*str)
-				str++;
+			free(*array);
+			*array = NULL;
+			return ;
 		}
+		j += index;
 	}
 }
 
@@ -77,6 +100,8 @@ int	ft_get_array(int **array, char **data, int len)
 	*array = ft_calloc(size, sizeof(int));
 	if (!*array)
 		return (-1);
-	ft_fill_array(data, *array, len, size);
+	ft_fill_array(data, array, len, size);
+	if (!*array)
+		return (-1);
 	return (size);
 }
