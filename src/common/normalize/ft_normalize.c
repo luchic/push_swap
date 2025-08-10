95462cf349d3d798d7c81ee9f4e232a78f308709
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-10 21:02:49 +0200

    Move filese to common folder

---
+++ src/common/normalize/ft_normalize.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_normalize.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 15:28:12 by nluchini          #+#    #+#             */
+/*   Updated: 2025/08/10 11:27:36 by nluchini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_get_weight(int num, int *array, int size)
+{
+	int	weight;
+	int	i;
+
+	weight = 0;
+	i = -1;
+	while (++i < size)
+	{
+		if (array[i] < num)
+			weight++;
+	}
+	return (weight);
+}
+
+int	*ft_normalize(int *array, int size)
+{
+	int	*normalized_array;
+	int	i;
+
+	if (!array || size <= 0)
+		return (NULL);
+	normalized_array = ft_calloc(size, sizeof(int));
+	if (!normalized_array)
+		return (NULL);
+	i = -1;
+	while (++i < size)
+	{
+		normalized_array[i] = ft_get_weight(array[i], array, size);
+	}
+	return (normalized_array);
+}
