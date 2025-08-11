d1532f6cc6d8840e1227cec5924435b257ff3479
Nikita Luchinin <nluchini@1-D-12.42heilbronn.de>
2025-08-11 10:41:22 +0200

    Edit ft_realloc.c: ft_calloc instead of malloc

--- libft/src/core/ft_realloc.c
+++ libft/src/core/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 10:28:07 by nluchini          #+#    #+#             */
-/*   Updated: 2025/08/10 20:58:50 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/10 21:33:42 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 	size_t	copy_size;
 
 	if (ptr == NULL)
-		return (malloc(new_size));
+		return (ft_calloc(new_size, sizeof(char)));
 	if (new_size == 0)
 		return (free(ptr), NULL);
 	str = (char *)ptr;
-	new_ptr = (char *)malloc(new_size * sizeof(char));
+	new_ptr = (char *)ft_calloc(new_size, sizeof(char));
 	if (new_ptr == NULL)
 		return (NULL);
 	if (old_size < new_size)
